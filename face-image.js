215414e66dc3c86200b4985f9a421539a1fdaca7
John Elliott <johnelliott703@gmail.com>
2015-10-27 22:00:16 -0400

    tabs -> spaces in face-image

--- face-image.js
+++ face-image.js
@@ -3,25 +3,25 @@
 var cv = require('opencv');
 
 cv.readImage("./images/john.jpg", function(err, im) {
-	if (err) {
-		console.error("Error:", err);
-		throw err;
-	}
-	if (im.width() < 1 || im.height() < 1) throw new Error('image has no size');
+  if (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+  if (im.width() < 1 || im.height() < 1) throw new Error('image has no size');
 
-	im.detectObject('node_modules/opencv/data/haarcascade_frontalface_alt.xml', {}, function(err, faces) {
-		if (err) {
-			console.error("Error:", err);
-			throw err;
-		}
+  im.detectObject('node_modules/opencv/data/haarcascade_frontalface_alt.xml', {}, function(err, faces) {
+    if (err) {
+      console.error("Error:", err);
+      throw err;
+    }
 
-		for (var i = 0; i < faces.length; i++) {
-			var face = faces[i];
-			im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
-		}
+    for (var i = 0; i < faces.length; i++) {
+      var face = faces[i];
+      im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+    }
 
-		im.save("./tmp/face.jpg");
+    im.save("./tmp/face.jpg");
 
-		console.log('done');
-	});
+    console.log('done');
+  });
 });
