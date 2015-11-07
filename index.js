b93c4f4fd5f3bd88945d6313190e512a0b317332
John Elliott <johnelliott703@gmail.com>
2015-11-07 15:44:12 -0500

    Remove visual feedback, move main detection to mod

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-var cv = require('opencv');
 var path = require('path');
-var loader = require('./lib/file.js');
-var encircle = require('./lib/circle.js');
+var detector = require('./lib/detector.js');
 
-var vid = loader(path.join(__dirname, 'movies', 'ped-clip-2.mov'));
+var videoPath = path.join(__dirname, 'movies', 'ped-clip-2.mov');
+console.log('video path', videoPath);
 
-var namedWindow = new cv.NamedWindow('Video', 1);
-
-var intervalId = setInterval(function() {
-  vid.read(function(err, mat) {
-    if (err) {
-      console.error("Error:", err);
-      throw err;
-    }
-
-    mat.detectObject('node_modules/opencv/data/haarcascade_fullbody.xml', {}, function(err, faces) {
-      if (err) {
-        console.error("Error:", err);
-        throw err;
-      }
-
-      // Draw a circle over the face
-      var circledMat = encircle(mat, faces);
-
-      // Draw the frame to the window
-      namedWindow.show(circledMat);
-
-      var res = namedWindow.blockingWaitKey(0, 50);
-      if (res >= 0) {
-        clearInterval(intervalId);
-      }
-
-    });
-  });
-}, 150);
-
+detector(videoPath, function(data) {
+  console.log('data', data);
+});
---
+++ repl.js
@@ -0,0 +1,11 @@
+var mod = require('./lib/annotator.js')
+mod
+var fs = require('fs')
+fs
+mod
+fs
+var mov = fs.createReadStream('../movies/ped-clip-2.mov');
+mov
+var path = require('path');
+path
+.vars
