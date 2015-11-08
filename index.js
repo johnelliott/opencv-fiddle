a29d2cfed3a3d434be96f7ff710f1196cc0c5fab
John Elliott <johnelliott703@gmail.com>
2015-11-07 23:12:40 -0500

    get some basic round trip working--wip

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
+var fs = require('fs');
 var path = require('path');
 var detector = require('./lib/detector.js');
+var http = require('http');
 
-var videoPath = path.join(__dirname, 'movies', 'ped-clip-2.mov');
-console.log('video path', videoPath);
+// Create server
+var server = http.createServer();
 
-detector(videoPath, function(data) {
-  console.log('data', data);
+server.listen(3000, function() {
+  console.log('listening on port 3000');
+});
+
+server.on('request', function(req, res) {
+  // TODO check content-type for posts
+
+  // create temp file
+  var tempPath = path.join(__dirname, 'tmp', 'file.mov');
+  var file = fs.createWriteStream(tempPath);
+  // pipe the data into a file
+  req.pipe(file);
+
+  // declare victory
+  req.on('data', function(data) {
+    console.log('we got data!:', data);
+  });
+
+  req.on('end', function() {
+    console.log('req end');
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+
+    // call detector on that file
+    detector(tempPath, function(err, data) {
+      if (err) {
+        // return instead of throwing an error here
+        return console.error(err);
+      }
+      console.log('processed:', data);
+      // send back the results
+      res.write(JSON.stringify(data), ['Transfer-Encoding', 'chunked']);
+    });
+
+  });
+
+  // clean up the temp file
+  res.on('finish', function deleteTmpFile() {
+    console.log('deleting', tempPath);
+    fs.unlink(tempPath, function (err) {
+      if (err) throw err;
+      console.log('successfully deleted temp file');
+    });
+  });
+
 });
